6cd881d0b1de33ec1829879d2914583b2661bc81
S.MURUGAN <104258298+MURUGANQA@users.noreply.github.com>
2024-09-05 20:16:43 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:psql@localhost/public'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:psql@127.0.0.1:5432/public'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:psql@localhost:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'super-secret'
